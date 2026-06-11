0e9668c5eb80ed26ef33bdb413c862042cb7d21c
Deuzimara Nunes <138496185+deuzimaranunes@users.noreply.github.com>
2026-06-11 17:27:57 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,17 +1,19 @@
 const inicio = new Date("2026-01-11");
 
-function atualizarTempo() {
+function atualizarTempo(){
 
     const agora = new Date();
 
     const diferenca = agora - inicio;
 
     const dias = Math.floor(
-        diferenca / (1000 * 60 * 60 * 24)
+        diferenca /
+        (1000*60*60*24)
     );
 
     document.getElementById("tempo")
-        .innerHTML = dias + " dias ❤️";
+    .innerHTML =
+    dias + " dias ❤️";
 }
 
 atualizarTempo();
@@ -23,31 +25,60 @@ setInterval(
 
 const motivos = [
 
-    "Seu sorriso ilumina meus dias ❤️",
+"Seu sorriso ilumina meus dias ❤️",
 
-    "Seu abraço é meu lugar favorito ❤️",
+"Seu abraço é meu lugar favorito ❤️",
 
-    "Você me faz feliz ❤️",
+"Você me faz feliz ❤️",
 
-    "Você transforma tudo em algo especial ❤️",
+"Você transforma tudo em algo especial ❤️",
 
-    "Você é meu amor ❤️"
+"Porque eu amo estar ao seu lado ❤️"
 
 ];
 
-function mostrarMotivo() {
+function mostrarMotivo(){
 
-    const indice = Math.floor(
-        Math.random() * motivos.length
-    );
+    const indice =
+    Math.floor(
+    Math.random()
+    * motivos.length);
 
     document.getElementById("motivo")
-        .innerHTML = motivos[indice];
+    .innerHTML =
+    motivos[indice];
 }
 
-if ("serviceWorker" in navigator) {
+const container =
+document.querySelector(
+".hearts-container"
+);
+
+for(let i=0;i<40;i++){
+
+    const heart =
+    document.createElement("div");
+
+    heart.classList.add("heart");
+
+    heart.innerHTML = "❤️";
+
+    heart.style.left =
+    Math.random()*100 + "%";
+
+    heart.style.fontSize =
+    (20 + Math.random()*25)
+    + "px";
+
+    heart.style.animationDelay =
+    Math.random()*10 + "s";
+
+    container.appendChild(heart);
+}
+
+if("serviceWorker" in navigator){
 
     navigator.serviceWorker.register(
-        "service-worker.js"
+    "service-worker.js"
     );
 }
