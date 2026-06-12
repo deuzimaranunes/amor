00f8ddc2cf4120ff710a3bd8cae93e744078eab7
Deuzimara Nunes <138496185+deuzimaranunes@users.noreply.github.com>
2026-06-12 00:27:48 -0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,38 +1,33 @@
-const CACHE = "amor-v1";
+const CACHE = "amor-v2";
+
+const ASSETS = [
+  "/",
+  "/index.html",
+  "/style.css",
+  "/script.js",
+  "/manifest.json",
+  "/icon-192.png",
+  "/icon-512.png"
+];
 
 self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE).then(cache => cache.addAll(ASSETS))
+  );
+  self.skipWaiting();
+});
 
-    event.waitUntil(
-
-        caches.open(CACHE)
-
-        .then(cache => {
-
-            return cache.addAll([
-                "/",
-                "/index.html",
-                "/style.css",
-                "/script.js"
-            ]);
-
-        })
-
-    );
-
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
 });
 
 self.addEventListener("fetch", event => {
-
-    event.respondWith(
-
-        caches.match(event.request)
-
-        .then(response => {
-
-            return response || fetch(event.request);
-
-        })
-
-    );
-
+  event.respondWith(
+    caches.match(event.request).then(cached => cached || fetch(event.request))
+  );
 });
